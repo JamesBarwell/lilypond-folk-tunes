43875cb5c6b10da543440a005d0135fe5b1f5bc4
James Barwell <jb@jamesbarwell.co.uk>
2024-08-17 14:43:16 +0100

    Add table of contents

--- bin/book.sh
+++ bin/book.sh
@@ -15,19 +15,26 @@ cat <<EOT >> ${TEMP_PATH}
   left-margin = 8
   right-margin = 8
 }
+
+\markuplist \table-of-contents
 EOT
 
 for FILEPATH in ./sets/*.ly; do
+TITLE=$(grep -E "title = .*" ${FILEPATH} | sed "s/title = //" | sed 's/"//g' | awk '{$1=$1};1' | sed "N;s/\n/, /")
+
 cat <<EOT >> ${TEMP_PATH}
 \bookpart {
+  \tocItem \markup "${TITLE}"
   \include "${FILEPATH}"
 }
 EOT
 done
 
 for FILEPATH in ./tunes/*.ly; do
+TITLE=$(grep -E "title = .*" ${FILEPATH} | sed "s/title = //" | sed 's/"//g' | awk '{$1=$1};1' | sed "N;s/\n/, /")
 cat <<EOT >> ${TEMP_PATH}
 \bookpart {
+  \tocItem \markup "${TITLE}"
   \include "${FILEPATH}"
 }
 EOT
